06c4450e7bb3ba0ffa224cbd966fe68b5429a6b1
elizabeth-oh <91884455+elizabeth-oh@users.noreply.github.com>
2022-01-12 18:17:39 +0100

    Integrated dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './Dictionary.css'
 
 export default function Dictionary() {
     const [keyword, setKeyword] = useState(""); 
 
+    function handleResponse(response) {
+        console.log(response.data[0]);
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
-        alert(`Searching for definition of ${keyword}`)
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleChange(event) {
-        setKeyword (event.target.value);
+        setKeyword (event.target.value)
     }
 
     return (
