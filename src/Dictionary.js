4d28b1b630cd3e5f79dc1ed0276652aca4c0563e
elizabeth-oh <91884455+elizabeth-oh@users.noreply.github.com>
2022-01-15 18:54:15 +0100

    Integrated rendering of word meanings and definitions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Results from './Results';
 import './Dictionary.css'
 
 export default function Dictionary() {
-    const [keyword, setKeyword] = useState(""); 
+    let [keyword, setKeyword] = useState(""); 
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) {
-        console.log(response.data[0]);
+        setResults(response.data[0]);
     }
 
     function handleSubmit(event) {
         event.preventDefault();
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
+        console.log(apiUrl);
         axios.get(apiUrl).then(handleResponse);
     }
 
@@ -25,6 +28,7 @@ export default function Dictionary() {
                 <input type = "search" placeholder = "search here" onChange = {handleChange}/>
                 <input type = "submit" value="Search" />
             </form>
+            <Results results={results} />
         </div>
     )
 }
---
+++ src/Meaning.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Meaning(props) {
+    return (
+        <div className="meaning">
+            <h3>{props.meaning.partOfSpeech}</h3>
+            {props.meaning.definitions.map(function(definition,index) {
+                return (
+                    <div key={index}>
+                        {definition.definition}
+                        <br />
+                        <em>
+                            {definition.example}
+                        </em>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
